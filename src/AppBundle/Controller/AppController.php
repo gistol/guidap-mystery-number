f8870055326d073c399e587a011bb610df56e2fe
Alix Mauro <alixmauro@gmail.com>
2017-08-12 17:27:25 +0200

    Add app controller for main vue

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -6,14 +6,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class MysteryNumberController extends Controller
+class AppController extends Controller
 {
     /**
-     * @Route("/api/mystery", name="mystery")
+     * @Route("/", name="homepage")
      */
-    public function mysteryNumberAction(Request $request)
+    public function AppAction(Request $request)
     {
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
