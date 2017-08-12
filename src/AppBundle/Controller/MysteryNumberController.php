d9e9da156c351fce288c7d110a7a957d8ca9769d
Alix Mauro <alixmauro@gmail.com>
2017-08-12 23:17:44 +0200

    Use MysteryNumberService and check parameter range in MysteryNumberController

--- src/AppBundle/Controller/MysteryNumberController.php
+++ src/AppBundle/Controller/MysteryNumberController.php
@@ -3,19 +3,29 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+use AppBundle\Service\MysteryNumberService;
 
 class MysteryNumberController extends Controller
 {
     /**
      * @Route("/api/mystery", name="mystery")
+     * @Method({"GET"})
      */
-    public function mysteryNumberAction(Request $request)
+    public function mysteryNumberAction(Request $request, MysteryNumberService $mysteryNumberService)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $guess = $request->query->getInt('guess', -1);
+
+        if ($guess < 0 || $guess > 100) {
+            throw new BadRequestHttpException("Parameter 'guess' is mandatory and must be an integer betwen 0 and 100.");
+        }
+
+        $result = $mysteryNumberService->play(intval($guess, 10));
+
+        return $this->json([ 'status' => true, 'data' => $result ]);
     }
 }
